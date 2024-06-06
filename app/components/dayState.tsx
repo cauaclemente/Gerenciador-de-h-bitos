fed74543d441da1e93b4b4802da051e5371e9fe1
Cauã Clemente <cauaclemente07@gmail.com>
2024-06-06 17:05:40 -0300

    feat: Creating the habit list

---
+++ app/components/dayState.tsx
@@ -0,0 +1,22 @@
+
+import { IoMdCheckmark } from "react-icons/io";
+import { TbPointFilled } from "react-icons/tb";
+import { TiDelete } from "react-icons/ti";
+
+const DayState = ({ day }: { day: boolean | undefined }) => {
+  // Função para retornar o ícone correto
+  const renderIcon = () => {
+    if (day === true) return <IoMdCheckmark color="#00ff9d" size={20} className="md:size-6"/>;
+    if (day === false) return <TiDelete color="red" size={20} className="md:size-6" />;
+    return <TbPointFilled color="#abbbd2" size={20} className="md:size-6"  />;
+  };
+
+  return (
+    <div className="flex justify-center items-center h-9">
+      {renderIcon()}
+    </div>
+  );
+};
+
+export default DayState;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+import DayState from "./components/dayState";
+
 
 export default function Home() {
 
-  const habits = {}
+  const habits = {
+    "Beber água": {
+      "2024-06-06": false,
+      "2024-06-05": true,
+      "2024-06-04": true,
+    },
+    "ler livro": {
+      "2024-06-06": true,
+      "2024-06-05": false,
+      "2024-06-04": true,
+    }
+  }
+
+  const weekDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
 
   return (
     <>
@@ -11,6 +27,29 @@ export default function Home() {
             Você não tem hábitos cadastrados
           </h1>
         ))}
+        {habits !== null &&
+          Object.entries(habits).map(([habit, habitStreak]) => (
+            <div key={habit} className="flex flex-col gap-2">
+              <div className=" flex justify-between items-center"> 
+                <span className="text-xl font-light text-white font-sans">
+                  {habit}
+                </span>
+                <button> <RiDeleteBin6Line color="red" size={20} />
+                </button>
+              </div>
+              <section className=" grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+              {weekDays.map((day) => (
+                <div key={day} className="flex flex-col" >
+                  <span className="font-sans text-xs text-white md:text-sm text-center">
+                  {day}
+                </span>
+                <DayState day={undefined} />
+                </div>
+              ))}
+              </section>  
+            </div>
+          ))
+        }
       </main>
     </>
   );
